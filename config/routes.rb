3b8dfb4a5e2d03cb24f4a67bdeda5d8a08b5ab8a
arakaki <arakaki@email.com>
2020-04-21 13:34:36 +0900

    progate chapter6

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  get   "users/:id/edit"       => "users#edit"
+  post  "users/:id/update"     => "users#update"
+  post  "users/create"         => "users#create"
+  get   "signup"               => "users#new"
+  get   'users/index'          => "users#index"
+  get   "users/:id"            => "users#show"
+
+
   get   "posts/index"          => "posts#index"
   get   "posts/new"            => "posts#new"
   post  "posts/create"         => "posts#create"
